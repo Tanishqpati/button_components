8b815ef357b3ec342d3b71a1d7757ee5f3601b9f
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-02-26 14:04:15 +0530

    Add button component on home screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{display:'flex', justifyContent:'space-between', alignItems: 'center'}}>
+      <Button type="primary" size="small">Button</Button>
+      <Button type="secondary" size="medium">Button</Button>
+      <Button type="text" size="large">Button</Button>
+      <Button type="elevated" size="large" disabled>Button</Button>
       </header>
     </div>
   );
