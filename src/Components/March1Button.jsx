1e1605cca65fe1e8fa37e4f4de2767ff8eccd758
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-03-04 16:42:29 +0530

    Add March1Button component with props validation and default props

---
+++ src/Components/March1Button.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import "./March1style.css";
+
+const BUTTON_TYPES = {
+  PRIMARY: "primary",
+  SECONDARY: "secondary",
+  TEXT: "text",
+  ELEVATED: "elevated",
+};
+
+const BUTTON_SIZES = {
+  SMALL: "small",
+  MEDIUM: "medium",
+  LARGE: "large",
+};
+
+const March1Button = ({ children, type, size, disabled, onClick }) => {
+  const buttonClasses = classNames(
+    "button",
+    `button-${type}`,
+    `button-${size}`,
+    {
+      "button-disabled": disabled,
+    }
+  );
+
+  const handleClick = () => {
+    if (!disabled && onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <button className={buttonClasses} onClick={handleClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+March1Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(Object.values(BUTTON_TYPES)).isRequired,
+  size: PropTypes.oneOf(Object.values(BUTTON_SIZES)).isRequired,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+March1Button.defaultProps = {
+  disabled: false,
+};
+
+export default March1Button;
